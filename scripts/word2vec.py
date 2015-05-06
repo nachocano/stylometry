a63ab056dd1bf356799dec959aea090cc82ba856
nachocano <nachoacano@gmail.com>
2015-05-06 00:08:52 -0700

    updating stuff

--- scripts/word2vec.py
+++ scripts/word2vec.py
@@ -18,7 +18,26 @@ def main():
   assert args.corpus == 'novels' or args.corpus == 'nyt'
 
   data, syn_data = utils.read_data(args.raw_folder, args.parsed_folder, args.corpus)
+  words2idx = data[1]
+  orig_size = len(words2idx)
+  words2idx_syn = syn_data[1]
+  idx2word  = dict((k,v) for v,k in words2idx.iteritems())
+  idx2word_syn  = dict((k,v) for v,k in words2idx_syn.iteritems())
 
+  for key in idx2word_syn:
+    idx2word[key + orig_size] = idx2word_syn[key]
+  words2idx  = dict((k,v) for v,k in idx2word.iteritems())
+
+  new_syn_data = []
+  for ids, novel in syn_data[0]:
+    new_novel = []
+    for sentence in novel:
+      new_novel.append(sentence + orig_size)
+    new_syn_data.append((ids, new_novel))
+
+  # assert
+  for d, s in zip(data[0], new_syn_data[0]):
+      assert d[0] == s[0]
 
 if __name__ == '__main__':
   main()
