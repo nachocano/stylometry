cd615e56ebfc5a8ba916a2c3005b7651872f114b
nachocano <nachoacano@gmail.com>
2015-05-18 18:16:06 -0700

    updating missing stuff

--- scripts/longest.py
+++ scripts/longest.py
@@ -9,18 +9,22 @@ from gensim import matutils
 from operator import itemgetter
 import re
 
-# 35152
+# 35152 max sentence
+# 15236 max words in a sentence
 def main():
   parser = argparse.ArgumentParser(description='TODO')
   parser.add_argument('-i', '--input_file', required=True)
   args = parser.parse_args()
 
   greatest = 0
+  best_line = None
   for line in open(args.input_file).read().splitlines():
     line_splitted = line.split()
     if len(line_splitted) > greatest:
       greatest = len(line_splitted)
+      best_line = line
   print 'biggest novel has %s sentences' % greatest
+  print 'line %s' % best_line
 
 if __name__ == '__main__':
   main()
--- scripts/missing_nyt.py
+++ scripts/missing_nyt.py
@@ -10,16 +10,18 @@ def main():
   parser.add_argument('-g', '--great_file', required=True)
   parser.add_argument('-t', '--typical_file', required=True)
   parser.add_argument('-v', '--very_good_file', required=True)
+  parser.add_argument('-go', '--great_file_out', required=True)
+  parser.add_argument('-to', '--typical_file_out', required=True)
+  parser.add_argument('-vo', '--very_good_file_out', required=True)
   args = parser.parse_args()
 
   labels = {'great' : 0, 'verygood' : 1, 'typical' : 2}
 
-  dictionary = {}
-  populate_dict(dictionary, args.great_file)
-  populate_dict(dictionary, args.typical_file)
-  populate_dict(dictionary, args.very_good_file)
+  great_dict = populate_dict(args.great_file)
+  typical_dict = populate_dict(args.typical_file)
+  very_good_dict = populate_dict(args.very_good_file)
 
-  print 'total files %d' % len(dictionary)
+  print 'total files %d' % (len(great_dict) + len(typical_dict) + len(very_good_dict))
 
   total_docs = 0
   for label in os.listdir(args.input_folder):
@@ -28,23 +30,45 @@ def main():
       continue
     lid = labels[label]
     for document in os.listdir(label_folder):
-      assert document in dictionary
-      dictionary[document] = True
+      if lid == 0:
+        assert document in great_dict
+        great_dict[document] = True
+      elif lid == 1:
+        assert document in very_good_dict
+        very_good_dict[document] = True
+      elif lid == 2:
+        assert document in typical_dict
+        typical_dict[document] = True
+      else: 
+        raise Exception
       total_docs += 1
 
   missing = 0
-  for k in dictionary:
-    if dictionary[k] == False:
-      print k
+  go = open(args.great_file_out, 'w')
+  to = open(args.typical_file_out, 'w')
+  vo = open(args.very_good_file_out, 'w')
+  for k in great_dict:
+    if great_dict[k] == False:
+      go.write('%s\n' % k)
+      missing +=1
+  for k in typical_dict:
+    if typical_dict[k] == False:
+      to.write('%s\n' % k)
+      missing +=1
+  for k in very_good_dict:
+    if very_good_dict[k] == False:
+      vo.write('%s\n' % k)
       missing +=1
   
-  print 'nenkova total %s' % len(dictionary)
+  print 'nenkova total %s' % (len(great_dict) + len(typical_dict) + len(very_good_dict))
   print 'total downloaded %s' % total_docs
   print 'missing files %s' % missing
 
-def populate_dict(dictionary, filename):
+def populate_dict(filename):
+  dictionary = {}
   for line in open(filename).read().splitlines():
     dictionary[line] = False
+  return dictionary
             
 if __name__ == '__main__':
   main()
